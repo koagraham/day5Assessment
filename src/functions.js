b90d970139d3c33472a892be05a148df1d4ac471
Koa Graham <koagraham233@gmail.com>
2024-04-26 13:15:57 -0600

    successful

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,9 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
+function divide(x, y) {
+  return x / 2
+}
 
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
@@ -10,39 +12,75 @@ function divide(x, y) {}
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+  return ((x + y) / 2)
+}
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
+function approximatelyEqual(x, y) {
+  if (Math.abs(Math.round(x)) === Math.abs(Math.round(y))) {
+    return true
+  }
+}
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+  return `${person} was drinking ${beverage} at ${location}.`
+}
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
-function censorVowels(string) {}
+function censorVowels(string) {
+  const result = []
+  let resultT = undefined
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u") {
+      result.push("*")
+    }
+    else {
+      result.push(string[i])
+    }
+    resultT = result.join('')
+  }
+  return resultT
+}
 
 // Return the given string in sticky case.
 // Ex.:
 //   stickyCase('hello world');
 //   => 'hElLo wOrLd'
-function stickyCase(string) {}
+function stickyCase(string) {
+  const result = []
+  let actualResult = undefined
+  for (let i = 1; i < string.length + 1; i++) {
+    if (i % 2 == 0) {
+      result.push(string[i-1].toUpperCase())
+    }
+    else {
+      result.push(string[i-1].toLowerCase())
+    }
+    actualResult = result.join('')
+  }
+  return actualResult
+}
 
 // Return the given string in leetspeak. Leetspeak is a modified version of
 // English where characters are replaced by numbers or symbols. For this
@@ -56,7 +94,35 @@ function stickyCase(string) {}
 // Ex.:
 //   leetspeak('javascript');
 //   => 'j4v45cr1p7'
-function leetspeak(string) {}
+function leetspeak(string) {
+  const result = []
+  let resultT = undefined
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "a") {
+      result.push(4)
+    }
+    else if (string[i] === "e") {
+      result.push(3)
+    }
+    else if (string[i] === "i") {
+      result.push(1)
+    }
+    else if (string[i] === "o") {
+      result.push(0)
+    }
+    else if (string[i] === "s") {
+      result.push(5)
+    }
+    else if (string[i] === "t") {
+      result.push(7)
+    }
+    else {
+      result.push(string[i])
+    }
+    resultT = result.join('')
+  }
+  return resultT
+}
 
 export {
   approximatelyEqual,
--- src/further-study.js
+++ src/further-study.js
@@ -6,14 +6,32 @@
 //   => ['a']
 //   commonStrings(['zoo', 'space', 'zoo'], ['zoo', 'space', 'boat']);
 //   => ['zoo', 'space']
-function commonStrings(array1, array2) {}
+function commonStrings(array1, array2) {
+    result = []
+    for (const item of array1) {
+        if (array2.includes(item)) {
+            result.push(item)
+            const index = array2.indexOf(item)
+            array2.splice(index, 1)
+        }
+    }
+    return result
+}
 
 // Given three numbers, return a list of numbers from 1 to 100 that are
 // divisible by at least one of a, b or c.
 // Ex.:
 //   divisibleByEither(50, 30, 29);
 //   => [29, 30, 50, 58, 60, 87, 90, 100]
-function divisibleByEither(a, b, c) {}
+function divisibleByEither(a, b, c) {
+    const result = []
+    for (let i = 1; i < 101; i++) {
+        if (i % a === 0 || i % b === 0 || i % c === 0) {
+            result.push(i)
+        }
+    }
+    return result
+}
 
 // Compress a string using the rules below and return the result. To compress a
 // string, replace consecutive duplicate characters with a number and the
@@ -27,6 +45,29 @@ function divisibleByEither(a, b, c) {}
 //   => '4s2b'
 //   compressString('ssssbbba');
 //   => '4s3ba'
-function compressString(string) {}
+function compressString(string) {
+    const result = []
+    let actualResult = undefined
+    let count = null
+    let tick = 0
+    let down = 0
+    console.log(string)
+    for (const letter of string) {
+      count = 0
+      while (letter === string[tick]) {
+        count += 1
+        tick += 1
+        }
+      if (count > 1) {
+        result.push(count)
+      }
+      if (string[string.indexOf(letter, down) - 1] != letter) {
+        result.push(letter)
+      }
+      down += 1
+    }
+    actualResult = result.join('')
+  return actualResult
+}
 
 export { commonStrings, compressString, divisibleByEither };
